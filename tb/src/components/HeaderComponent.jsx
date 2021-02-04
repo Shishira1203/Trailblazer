6770687148b50c38710fff065854bba033b0771e
Shishira1203 <shishir.karanth@gmail.com>
2021-02-04 11:24:01 +0530

    Remove old files

--- tb/src/components/HeaderComponent.jsx
+++ tb/src/components/HeaderComponent.jsx
@@ -44,7 +44,7 @@ const Example = (props) => {
                         </NavItem>
                     </Nav>
                 </Collapse>
-            </Navbar>
+            </Navbar>   
         </div>
     );
 }
